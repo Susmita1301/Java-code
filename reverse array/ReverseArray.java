ed153879f2d11a05ffe9f0d16c5b58d6cd998366
Susmita Sarkar <sarkarsusmita1301@gmail.com>
2025-03-01 09:48:49 +0530

    Reverse array code

--- reverse array/ReverseArray.java
+++ reverse array/ReverseArray.java
@@ -11,31 +11,22 @@ public class ReverseArray {
             right--;
         }
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter space-separated numbers:");
         String[] input = scanner.nextLine().split(" ");
         int n = input.length;
         int[] arr = new int[n];
-
-        // Convert input to integer array
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(input[i]);
         }
         scanner.close();
-
-        // Reverse the array
         reverseArray(arr);
-
-        // Find max and min
         int max = arr[0], min = arr[0];
         for (int i = 1; i < n; i++) {
             if (arr[i] > max) max = arr[i];
             if (arr[i] < min) min = arr[i];
         }
-
-        // Print results
         System.out.print("Reversed Array: ");
         for (int num : arr) System.out.print(num + " ");
         System.out.println("\nMax Value: " + max);
